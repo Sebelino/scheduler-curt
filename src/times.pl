588430014e721fa24552aef873fb3f4e490dc7eb
Sebastian Olsson <sebelino@hotmail.com>
2014-12-01 20:46:49 +0100

    Added support for minutes (except padded zeroes).

--- src/times.pl
+++ src/times.pl
@@ -1,8 +1,15 @@
+%
 
-%timeEntry('7am').
 timeEntry(Time) :-
-    between(1,12,N),
-    atom_number(AN,N),
+    between(1,12,Hour),
+    atom_number(AtomHour,Hour),
     member(Period,['am','pm']),
-    atom_concat(AN,Period,Time).
+    atom_concat(AtomHour,Period,Time).
 
+timeEntry(Time) :-
+    between(1,12,Hour),
+    atom_number(AtomHour,Hour),
+    between(0,59,Minute),
+    atom_number(AtomMinute,Minute),
+    member(Period,['am','pm']),
+    atomic_list_concat([AtomHour,':',AtomMinute,Period],Time).
